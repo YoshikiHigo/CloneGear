a7b80ce3876464b7893494a4ae00d93512913e40
higo <higo@192.168.100.5>
2015-10-27 00:11:02 +0900

    files in bellon benchmark were renamed.

--- CloneGear/bellon/detect.sh
+++ CloneGear/bellon/detect.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-ant/src -thd 3 -result higo-eclipse-ant.txt -thrld 30 -gap 1 -bellon -module
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-ant/src -thd 3 -result higo-eclipse-ant2.txt -thrld 30 -gap 1 -bellon 
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-jdtcore/src -thd 3 -result higo-eclipse-jdtcore.txt -thrld 30 -gap 1 -bellon -module
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-jdtcore/src -thd 3 -result higo-eclipse-jdtcore2.txt -thrld 30 -gap 1 -bellon 
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/netbeans-javadoc/src -thd 3 -result higo-netbeans-javadoc.txt -thrld 30 -gap 1 -bellon -module
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/netbeans-javadoc/src -thd 3 -result higo-netbeans-javadoc2.txt -thrld 30 -gap 1 -bellon 
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/j2sdk1.4.0-javax-swing/src -thd 3 -result higo-j2sdk1.4.0-javax-swing.txt -thrld 30 -gap 1 -bellon -module
-java -ea -Xmx4g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/j2sdk1.4.0-javax-swing/src -thd 3 -result higo-j2sdk1.4.0-javax-swing2.txt -thrld 30 -gap 1 -bellon 
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-ant.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-ant2.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-jdtcore.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-jdtcore2.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-netbeans-javadoc.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-netbeans-javadoc2.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-j2sdk1.4.0-javax-swing.txt
-perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-j2sdk1.4.0-javax-swing2.txt
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-ant/src -thd 3 -result higo-eclipse-ant.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-ant/src -thd 3 -result higo-eclipse-ant-nofolding.cpf -thrld 30 -gap 1 -bellon -folding
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-jdtcore/src -thd 3 -result higo-eclipse-jdtcore.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/eclipse-jdtcore/src -thd 3 -result higo-eclipse-jdtcore-nofolding.cpf -thrld 30 -gap 1 -bellon -folding
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/netbeans-javadoc/src -thd 3 -result higo-netbeans-javadoc.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/netbeans-javadoc/src -thd 3 -result higo-netbeans-javadoc-nofolding.cpf -thrld 30 -gap 1 -bellon -folding
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/j2sdk1.4.0-javax-swing/src -thd 3 -result higo-j2sdk1.4.0-javax-swing.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/j2sdk1.4.0-javax-swing/src -thd 3 -result higo-j2sdk1.4.0-javax-swing-nofolding.cpf -thrld 30 -gap 1 -bellon -folding
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/cook/src -thd 3 -result higo-cook.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/snns/src -thd 3 -result higo-snns.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/postgresql/src -thd 3 -result higo-postgresql.cpf -thrld 30 -gap 1 -bellon 
+java -ea -Xmx12g -jar CGFinder.jar -src /Users/higo/Desktop/data/versions/bellon/weltab/src -thd 3 -result higo-weltab.cpf -thrld 30 -gap 1 -bellon 
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-ant.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-ant-nofolding.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-jdtcore.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-eclipse-jdtcore-nofolding.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-netbeans-javadoc.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-netbeans-javadoc-nofolding.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-j2sdk1.4.0-javax-swing.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-j2sdk1.4.0-javax-swing-nofolding.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-cook.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-snns.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-postgresql.cpf
+perl -pi -e "s/\/Users\/higo\/Desktop\/data\/versions\/bellon\///g" higo-weltab.cpf
--- CloneGear/bellon/eval.sh
+++ CloneGear/bellon/eval.sh
@@ -1,24 +1,44 @@
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones higo-eclipse-ant.txt -threshold 0.7 -results eclipse-ant.csv -newfile
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones higo-eclipse-ant2.txt -threshold 0.7 -results eclipse-ant.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones baker-eclipse-ant.txt -threshold 0.7 -results eclipse-ant.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones baxter-eclipse-ant.txt -threshold 0.7 -results eclipse-ant.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones kamiya-eclipse-ant.txt -threshold 0.7 -results eclipse-ant.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.txt -clones merlo-eclipse-ant.txt -threshold 0.7 -results eclipse-ant.csv
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones higo-eclipse-jdtcore.txt -threshold 0.7 -results eclipse-jdtcore.csv -newfile
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones higo-eclipse-jdtcore2.txt -threshold 0.7 -results eclipse-jdtcore.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones baker-eclipse-jdtcore.txt -threshold 0.7 -results eclipse-jdtcore.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones baxter-eclipse-jdtcore.txt -threshold 0.7 -results eclipse-jdtcore.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones kamiya-eclipse-jdtcore.txt -threshold 0.7 -results eclipse-jdtcore.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.txt -clones merlo-eclipse-jdtcore.txt -threshold 0.7 -results eclipse-jdtcore.csv
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones higo-j2sdk1.4.0-javax-swing.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv -newfile
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones higo-j2sdk1.4.0-javax-swing2.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones baker-j2sdk1.4.0-javax-swing.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones baxter-j2sdk1.4.0-javax-swing.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones kamiya-j2sdk1.4.0-javax-swing.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.txt -clones merlo-j2sdk1.4.0-javax-swing.txt -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones higo-netbeans-javadoc.txt -threshold 0.7 -results netbeans-javadoc.csv -newfile
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones higo-netbeans-javadoc2.txt -threshold 0.7 -results netbeans-javadoc.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones baker-netbeans-javadoc.txt -threshold 0.7 -results netbeans-javadoc.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones baxter-netbeans-javadoc.txt -threshold 0.7 -results netbeans-javadoc.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones kamiya-netbeans-javadoc.txt -threshold 0.7 -results netbeans-javadoc.csv 
-java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.txt -clones merlo-netbeans-javadoc.txt -threshold 0.7 -results netbeans-javadoc.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones higo-eclipse-ant.cpf -threshold 0.7 -results eclipse-ant.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones higo-eclipse-ant-nofolding.cpf -threshold 0.7 -results eclipse-ant.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones baker-eclipse-ant.cpf -threshold 0.7 -results eclipse-ant.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones baxter-eclipse-ant.cpf -threshold 0.7 -results eclipse-ant.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones kamiya-eclipse-ant.cpf -threshold 0.7 -results eclipse-ant.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-ant.cpf -clones merlo-eclipse-ant.cpf -threshold 0.7 -results eclipse-ant.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones higo-eclipse-jdtcore.cpf -threshold 0.7 -results eclipse-jdtcore.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones higo-eclipse-jdtcore-nofolding.cpf -threshold 0.7 -results eclipse-jdtcore.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones baker-eclipse-jdtcore.cpf -threshold 0.7 -results eclipse-jdtcore.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones baxter-eclipse-jdtcore.cpf -threshold 0.7 -results eclipse-jdtcore.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones kamiya-eclipse-jdtcore.cpf -threshold 0.7 -results eclipse-jdtcore.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle eclipse-jdtcore.cpf -clones merlo-eclipse-jdtcore.cpf -threshold 0.7 -results eclipse-jdtcore.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones higo-j2sdk1.4.0-javax-swing.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones higo-j2sdk1.4.0-javax-swing-nofolding.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones baker-j2sdk1.4.0-javax-swing.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones baxter-j2sdk1.4.0-javax-swing.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones kamiya-j2sdk1.4.0-javax-swing.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle j2sdk1.4.0-javax-swing.cpf -clones merlo-j2sdk1.4.0-javax-swing.cpf -threshold 0.7 -results j2sdk1.4.0-javax-swing.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones higo-netbeans-javadoc.cpf -threshold 0.7 -results netbeans-javadoc.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones higo-netbeans-javadoc-nofolding.cpf -threshold 0.7 -results netbeans-javadoc.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones baker-netbeans-javadoc.cpf -threshold 0.7 -results netbeans-javadoc.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones baxter-netbeans-javadoc.cpf -threshold 0.7 -results netbeans-javadoc.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones kamiya-netbeans-javadoc.cpf -threshold 0.7 -results netbeans-javadoc.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle netbeans-javadoc.cpf -clones merlo-netbeans-javadoc.cpf -threshold 0.7 -results netbeans-javadoc.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle cook.cpf -clones higo-cook.cpf -threshold 0.7 -results cook.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle cook.cpf -clones baker-cook.cpf -threshold 0.7 -results cook.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle cook.cpf -clones baxter-cook.cpf -threshold 0.7 -results cook.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle cook.cpf -clones kamiya-cook.cpf -threshold 0.7 -results cook.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle cook.cpf -clones merlo-cook.cpf -threshold 0.7 -results cook.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle snns.cpf -clones higo-snns.cpf -threshold 0.7 -results snns.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle snns.cpf -clones baker-snns.cpf -threshold 0.7 -results snns.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle snns.cpf -clones baxter-snns.cpf -threshold 0.7 -results snns.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle snns.cpf -clones kamiya-snns.cpf -threshold 0.7 -results snns.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle snns.cpf -clones merlo-snns.cpf -threshold 0.7 -results snns.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle postgresql.cpf -clones higo-postgresql.cpf -threshold 0.7 -results postgresql.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle postgresql.cpf -clones baker-postgresql.cpf -threshold 0.7 -results postgresql.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle postgresql.cpf -clones baxter-postgresql.cpf -threshold 0.7 -results postgresql.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle postgresql.cpf -clones kamiya-postgresql.cpf -threshold 0.7 -results postgresql.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle postgresql.cpf -clones merlo-postgresql.cpf -threshold 0.7 -results postgresql.csv
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle weltab.cpf -clones higo-weltab.cpf -threshold 0.7 -results weltab.csv -newfile
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle weltab.cpf -clones baker-weltab.cpf -threshold 0.7 -results weltab.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle weltab.cpf -clones baxter-weltab.cpf -threshold 0.7 -results weltab.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle weltab.cpf -clones kamiya-weltab.cpf -threshold 0.7 -results weltab.csv 
+java -ea -Xmx4g -jar CGEvaluator.jar -oracle weltab.cpf -clones merlo-weltab.cpf -threshold 0.7 -results weltab.csv
